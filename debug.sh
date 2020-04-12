37e82e63ae02963f1098d845a7255420c59cb45e
Vivien Richter <vivien-richter@outlook.de>
2020-04-12 05:20:03 +0200

    Workaround for mouse support added.

--- debug.sh
+++ debug.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PROJECT="$(echo "$1" | awk '{print tolower($0)}')"
-dosemu -t -m -D-a -I 'keystroke "cd C:\\PROJECTS\\'$PROJECT'\rC:\\TASM\\TD.EXE '$PROJECT'\r"'
+dosemu -D-a -I 'keystroke "cd C:\\PROJECTS\\'$PROJECT'\rC:\\TASM\\TD.EXE '$PROJECT'\r"'
 exit 0
 
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PROJECT="$(echo "$1" | awk '{print tolower($0)}')"
 tqc $PROJECT
-dosemu -t -m -D-a -I 'keystroke exitemu' -E $PROJECT.com
+dosemu -D-a -I 'keystroke exitemu' -E $PROJECT.com
 exit 0
